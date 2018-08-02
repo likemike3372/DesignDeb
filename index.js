6c3bd77c3d793e5bb22248a8a5232e0c5ea7b1b1
Mikhael Z Thomas <mikhael333@gmail.com>
2018-08-02 02:41:50 +0000

    add mongolab

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ app.use(flash());
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp_v3', { useNewUrlParser: true }); 
-
-
+//mongoose.connect('mongodb://localhost:27017/yelp_camp_v3', { useNewUrlParser: true }); 
+//mongodb://mikhael:jesuslord@ds051943.mlab.com:51943/yelpcamp
+mongoose.connect("mongodb://mikhael:jesuslord198@ds051943.mlab.com:51943/yelpcamp",{ useNewUrlParser: true });
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
     res.locals.error=req.flash("error");
